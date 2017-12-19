e2c7bc3cc7a4bc35ee59aae7716e1db51b84f819
wangyichen1064431086 <1064431086@qq.com>
2017-12-19 22:03:51 +0800

    1219learn

--- src/index.js
+++ src/index.js
@@ -536,19 +536,55 @@ function FancyBorder(props) {
   )
 }
 
-function WelcomeDialog() {
+
+function Dialog(props) {
   return (
     <FancyBorder color="blue">
       <h1 className="Dialog-title">
-        Welcome
+        {props.title}
       </h1>
       <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
+        {props.message}
       </p>
+      {props.children}
     </FancyBorder>
-  )
+  );
 }
 
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {
+      login: ''
+    };
+  }
+  handleChange(e) {
+    this.setState({
+      login: e.target.value
+    });
+  }
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program" message="How should we refer to you?">
+        <input value={this.state.login} onChange={this.handleChange} />
+        <button onClick={this.handleSignUp} >
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+}
+
+function WelcomeDialog() {
+  return (
+    <Dialog title="Welcome" message="Thank you for visiting our spacecraft!" />
+  );
+}
 function SplitPane(props) {
   return (
     <div className="SplitPane">
@@ -593,6 +629,7 @@ class MyRoot extends React.Component {
         <Reservation />
         <Calculator />
         <WelcomeDialog />
+        <SignUpDialog />
         <App />
       </div>
     )
