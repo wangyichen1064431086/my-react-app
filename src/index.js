3efd67a357075945c7e36fb29f867a909b89d65f
wangyichen1064431086 <1064431086@qq.com>
2017-12-20 23:07:36 +0800

    1220learn

--- src/index.js
+++ src/index.js
@@ -93,8 +93,6 @@ class Greeting extends React.Component {
   }
 }
 
-
-
 class LoginButton extends React.Component {
   render() {
     return (
@@ -609,6 +607,43 @@ function App() {
   return (
     <SplitPane left={<Contacts />} right={<Chat />} />
   );
+}
+
+
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">
+          {category}
+        </th>
+      </tr>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? product.name : <span style={{color: 'red'}}>
+      {product.name}
+    </span>;//如果库存为false就显示为红色。
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    )
+
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+
+  }
 }
 
 class MyRoot extends React.Component {
